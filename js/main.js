975e74045fd83fa25d4b393318ce3df33294cf9b
Cary Duncan <duncan.cary@gmail.com>
2015-10-26 23:09:36 -0400

    Fixed main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,33 @@
 import $ from 'jquery';
 import _ from 'underscore';
 import moment from 'moment';
+import Backbone from 'backbone';
+import CarsCollection from './cars_collection';
+import CarTemplate from './car_template';
+
+$.ajaxSetup({
+  headers: {
+    'X-Parse-Application-Id': 'N8u4Yx2ldwhjVvQHmTmJ8vybwfjFkzeI9iHlkm2q',
+    'X-Parse-REST-API-Key': 'w87J8Fm5OmuQz4ZYWaWT4BFuPrUaR2vzi0MGC1af'
+  }
+});
+
+let cars = new CarsCollection();
+
+function renderCars() {
+  let $ul = $('<ul></ul>');
+  cars.each(function(car){
+    let data = car.toJSON();
+    console.log('data', data);
+    let templateString = CarTemplate(data);
+    console.log('templateString', templateString);
+    let $li = $(templateString);
+    $ul.append($li);
+  });
+  $('body').html($ul);
+}
+
+cars.fetch().then(renderCars);
+
 
 console.log('Hello, World');
